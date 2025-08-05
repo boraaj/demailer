1b043fb8c09bcb99e8d269bb9ccd56551c3e83f9
boraaj <borja_bza@hotmail.com>
2025-08-05 12:46:48 +0200

    Interactive Menu
    Starting Interactive Menu

--- demailer.py
+++ demailer.py
@@ -1,13 +1,37 @@
 import email
-import sys
 import re
+import os
+import platform
 from tabulate import tabulate
+import pyfiglet
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from InquirerPy.separator import Separator
+from InquirerPy.validator import PathValidator
+
+
+def title():
+    ascii_title = pyfiglet.figlet_format("demailer", font='slant')
+    print(ascii_title)
+
+
+def clean_screen():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
 
 
 def analyze_header(eml_path):
     """
     Analize the headers from a .eml file and displays a security report
     """
+
+    clean_screen()
+
+    if not eml_path.find(".eml"):
+        print(f"❌ Error: File '{eml_path}' not found ❌ ")
+        return
 
     try:
         with open(eml_path, 'r', encoding='utf-8') as f:
@@ -43,12 +67,13 @@ def analyze_header(eml_path):
 
     if received_headers:
         for i, header in enumerate(received_headers):
-            ips = re.findall(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', header)
+            ips = re.findall(
+                r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', header)
             ip_str = ", ".join(ips) if ips else "IP Not Found"
             server_info = header.split(";")[0].replace('\n', ' ').strip()
             email_route.append([f"Hop {i+1}", server_info, ip_str])
         print(tabulate(email_route, headers=[
-              "Hop", "Server", "IPs"], tablefmt="grid", maxcolwidths=[None, 70, None]))
+            "Hop", "Server", "IPs"], tablefmt="grid", maxcolwidths=[None, 70, None]))
     else:
         print("'Received' Headers not found")
 
@@ -92,13 +117,40 @@ def analyze_header(eml_path):
         ["DMARC (Domain-based Message Auth)", dmarc_status]
     ]
 
-    print(tabulate(auth_table, headers=["Check", "Result"], tablefmt="grid"))
+    print(tabulate(auth_table, headers=[
+        "Check", "Result"], tablefmt="grid"))
     print("\n" + "="*50)
 
 
+def main():
+    # Menu function.
+    action = inquirer.select(
+        message="Select the option: ",
+        choices=[
+            "Header Analyzer",
+            Choice(value=None, name="Exit")
+        ],
+        default=None,
+    ).execute()
+
+    if action == "Header Analyzer":
+        # Providing .eml path
+        home_path = "." if os.name == "posix" else "."
+        src_path = inquirer.filepath(
+            message="Please, provide the path to the .eml file: ",
+            default=home_path,
+            validate=PathValidator(
+                is_file=True, message="Input is not a file"),
+            only_files=False,
+        ).execute()
+        analyze_header(src_path)
+
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Use: python demailer.py <path_to_eml_file>")
-    else:
-        eml_file = sys.argv[1]
-        analyze_header(eml_file)
+    title()
+    main()
+    # if len(sys.argv) < 2:
+    #     print("Use: python demailer.py <path_to_eml_file>")
+    # else:
+    #     eml_file = sys.argv[1]
+    #     analyze_header(eml_file)
